059db7ab9dcf81ab806cd5dcd2a0b996855f9001
Yerassil <yerassil@gmail.com>
2019-09-19 14:49:03 +0600

    Add user authentication and login form

--- django_social/urls.py
+++ django_social/urls.py
@@ -22,4 +22,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomeView.as_view(), name='home'),
+    path('login/', views.login_view, name='login')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django_social/views.py
+++ django_social/views.py
@@ -1,8 +1,25 @@
 from django.views.generic import ListView
 from .models import Image
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
 
 class HomeView(ListView):
     model = Image
     context_object_name = 'images'
     template_name = 'home.html'
+
+
+def login_view(request):
+    if request.method == 'POST':
+        user = authenticate(
+            username=request.POST.get('username'),
+            password=request.POST.get('password')
+        )
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect("/")
+        else:
+            return HttpResponseRedirect("/")
+    return render(request, "/")
